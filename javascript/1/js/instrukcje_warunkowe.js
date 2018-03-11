28e4a38120c9ffb5d30e85ae22bca8f5517959f3
Adrian Bak <adbak97@o2.pl>
2018-03-11 15:21:41 +0100

    cwiczenia ciag dalszy

---
+++ javascript/1/js/instrukcje_warunkowe.js
@@ -0,0 +1,44 @@
+////instrukcja if
+//var zmienna1 = "Kot";
+//var zmienna2 = "Pies";
+///*
+//if (zmienna1 == zmienna2) {
+//	console.log("Spełniam warunek")
+//}
+////instrukcja if else
+//if (zmienna1 == zmienna2) {
+//	console.log("Spełniam warunek")
+//} else {
+//	console.log("nie spełniam warunku")
+//}
+////Instrukcja else if
+//*/
+//
+//
+//var mojaLiczba = 112;
+//
+//if (mojaLiczba == 5) {
+//	console.log("Moja liczba to : 5")
+//} else if (mojaLiczba == 12) {
+//	console.log("Moja liczba to 12")
+//} else if (mojaLiczba == 122) {
+//	console.log("MOja liczba to 122")
+//} else if (mojaLiczba == 112) {
+//	console.log("MOja liczba to 112")
+//} else {
+//	console.log("Nie ma takich liczb")
+//}
+//
+//
+//
+//instrukcja switch
+
+var kolor = "zielony";
+switch (kolor) {
+	case "czerwony":
+		console.log("kolor czerwony")
+		break;
+	case "zielony":
+		console.log("kolor zielony");
+		break;
+}
