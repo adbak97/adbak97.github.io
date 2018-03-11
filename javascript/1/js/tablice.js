ee42092d6c2259b8b4bdbfffa8b148f05e44aa0b
Adrian Bak <adbak97@o2.pl>
2018-03-11 11:40:42 +0100

    Tablice javascript

---
+++ javascript/1/js/tablice.js
@@ -0,0 +1,39 @@
+var imiona = ['Adam', 'Marta', 'Jadwiga', 'Przemyłsaw', 'Kamil', 'Józef'];
+console.log(imiona[0], imiona[3]);
+//Dodawanie elementów  na koniec tablicy
+imiona[6] = "Mikołaj";
+imiona[7] = "Kinga";
+console.log(imiona);
+imiona.push('Bogumił');
+console.log(imiona);
+//Usuwanie elementów
+imiona.pop();
+//Dodawnie nowych elementów na poczatek tablicy 
+imiona.unshift('Małgosia', 'Wenowefa')
+console.log(imiona);
+var usunietyElement = imiona.shift();
+//console.log(usunietyElement);
+console.log(imiona);
+var iloscElementowTablicy = imiona.length;
+console.log(iloscElementowTablicy);
+console.log(imiona);
+//var textZTablicy = imiona[0] + imiona[1] + imiona[2];
+//console.log(textZTablicy);
+//rozbijanie tablicy na ciąg tekstowy
+var elementyTablicyString = imiona.join();
+console.log(elementyTablicyString);
+// join(", ") - domyślny separator to spacja ! 
+console.log(imiona);
+//Odwrócenie tablicy
+
+imiona.reverse();
+console.log(imiona);
+
+//Sortowanie tablicy
+imiona.sort();
+console.log(imiona);
+//Sprawdzanie czy wartość w tablicy istnieje
+console.log(imiona.indexOf("Kamil"));
+console.log(imiona.indexOf("Czesio"));
+console.log(imiona);
+imiona.slice(from: 1, to: 2);
