81c2049a5ff21fbe11d16c504684d2d30c3b0331
Adrian Bak <adbak97@o2.pl>
2018-03-18 12:22:54 +0100

    jquery

--- Jquery/Jquery - wprowadzenie/js/jquery.js
+++ Jquery/Jquery - wprowadzenie/js/jquery.js
@@ -1,8 +1,12 @@
+//document.ready gwarantuje ze skrypty wywolaja sie w momencie gdy caly model DOM jest juz zaladowany
+
+//ponizej skrocony zapis document.ready
 $(function () {
 
 	$("h2").css({
 		color: "red",
-		backgroundColor: "yellow"
+		backgroundColor: "yellow",
+
 
 	})
 
