d40c55fe5b51d7dd9cefbed05cda76d6b18978c2
Adrian Bak <adbak97@o2.pl>
2018-03-19 23:41:59 +0100

    kalkulator dalsze dzialania

--- zadania-domowe/zadanie-12/js/js.js
+++ zadania-domowe/zadanie-12/js/js.js
@@ -17,23 +17,37 @@ var liczba6 = document.getElementById('6');
 var liczba7 = document.getElementById('7');
 var liczba8 = document.getElementById('8');
 var liczba9 = document.getElementById('9');
+var flaga = 0; //w zaleznosci od liczby, program bedzie wiedział jakie zadanko arytmetyczne wykonac
 console.log(del);
 console.log(display);
 del.addEventListener("click", function (e) {
 	display.innerHTML = '';
 });
 procent.addEventListener("click", function (e) {
-	display.innerHTML = '%';
+
+	alert("EEEE Synek procenty nie działają");
+
 });
 dodawanie.addEventListener("click", function (e) {
-	display.innerHTML = '+';
+	var oldContent = display.innerHTML; //tutaj w jquery mozna by użyć appendChild(), albo insertBefore
+	console.log(oldContent);
+	console.log(typeof oldContent);
+	//Number(x);
+	display.innerHTML = oldContent + ' +';
+	flaga = 1;
 });
 odejmowanie.addEventListener("click", function (e) {
-	display.innerHTML = '-';
+	var oldContent = display.innerHTML;
+	display.innerHTML = oldContent + ' -';
+	flaga = 2;
 });
 mnozenie.addEventListener("click", function (e) {
-	display.innerHTML = '*';
+	var oldContent = display.innerHTML;
+	display.innerHTML = oldContent + ' *';
+	flaga = 3;
 });
 dzielenie.addEventListener('click', function (e) {
-	display.innerHTML = ':';
+	var oldContent = display.innerHTML;
+	display.innerHTML = oldContent + ' :';
+	flaga = 4;
 });
