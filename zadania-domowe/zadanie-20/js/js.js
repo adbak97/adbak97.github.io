8115ab509139cb1c91702df8d85f136ffd70b964
Adrian Bak <adbak97@o2.pl>
2018-04-16 19:01:29 +0200

    d

--- zadania-domowe/zadanie-20/js/js.js
+++ zadania-domowe/zadanie-20/js/js.js
@@ -1,28 +1,49 @@
 function data() {
+	var workers = [];
+
 	for (var i = 1; i <= 20; i++) {
 		var w = document.getElementById("pracownik" + i);
 		var workHours = w.querySelector(".czas").value;
 		var perHour = w.querySelector(".stawka").value;
 		var wyplata = w.querySelector(".wyplata");
+		var godnosc = w.querySelector(".pracownik").innerHTML;
 		var salary = 0;
+		var workersSalary = [];
 
-		function theBest() {
-			var salaryWorkers = document.querySelectorAll(".wyplata");
-			console.log(salaryWorkers);
-			console.log("salaryWorkers");
+
+
+		function Pracownicy() {
+
+			//konstruktor obiektu pracownik
+
+			function Worker(godnosc, godziny) {
+				this.godnosc = godnosc;
+				this.godziny = workHours;
+			}
+			// tworzenie pracownikow - obiektu, tworzenie tablicy obiektow
+			workers[i]= new Worker+i(godnosc, workHours);
 		}
+		
+		function trzechNajlepszych{
+			
+			
+			
+			
+			
+		}
+
 
 		if (workHours >= 100 && workHours <= 160) {
 			salary = workHours * perHour;
 			wyplata.innerText = salary;
-			theBest();
+			Pracownicy();
 		}
 		if (workHours > 160) {
 			var nadGodzinki = (workHours - 160) * perHour;
 
 			salary = (workHours * perHour) + nadGodzinki;
 			wyplata.innerText = salary;
-			theBest();
+			Pracownicy();
 		}
 		if (workHours <= 100) {
 			salary = workHours * perHour;
@@ -32,20 +53,27 @@ function data() {
 			w.querySelector(".wyplata").style.background = "red";
 			w.querySelector(".stawka").style.background = "red";
 			w.querySelector(".czas").style.background = "red";
-			theBest();
+			Pracownicy();
 
 		}
 
-		function theBest() {
-			var employeesSalary = document.querySelector(".wyplata").value;
-			console.log(employeesSalary);
-		}
+
 	}
+
+	console.log(workers);
+	console.log(workers[2]);
+
+
+
+
+
 
 
 }
-/*console.log(workHours);
-console.log(perHour);*/
+
+
+
+
 
 
 
