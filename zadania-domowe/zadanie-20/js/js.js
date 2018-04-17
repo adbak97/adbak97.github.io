28794eacb3f72f73c8ae3a70d3b44847c1de01f0
Adrian Bak <adbak97@o2.pl>
2018-04-18 00:07:30 +0200

    modyfikacja funckji js

--- zadania-domowe/zadanie-20/js/js.js
+++ zadania-domowe/zadanie-20/js/js.js
@@ -1,56 +1,82 @@
-function data() {
-	for (var i = 1; i <= 20; i++) {
-		var w = document.getElementById("pracownik" + i);
-		var workHours = w.querySelector(".czas").value;
-		var perHour = w.querySelector(".stawka").value;
-		var wyplata = w.querySelector(".wyplata");
-		var salary = 0;
+var pracownik;
+var czas;
+var stawka;
+var wyplata;
+var pensja;
+var w;
+var workers = [];
 
-		function theBest() {
-			var salaryWorkers = document.querySelectorAll(".wyplata");
-			console.log(salaryWorkers);
-			console.log("salaryWorkers");
+function oblicz() {
+	for (var i = 0; i < 20; i++) {
+		w = document.getElementById("pracownik" + (i + 1));
+		czas = w.querySelector(".czas").value;
+		stawka = w.querySelector(".stawka").value;
+		wyplata = w.querySelector(".wyplata");
+		pracownik = w.querySelector(".pracownik").innerHTML;
+		pensja = 0;
+		if (czas >= 100 && czas <= 160) {
+			pensja = czas * stawka;
+			wyplata.innerText = pensja;
 		}
-
-		if (workHours >= 100 && workHours <= 160) {
-			salary = workHours * perHour;
-			wyplata.innerText = salary;
-			theBest();
+		if (czas > 160) {
+			var nadGodzinki = (czas - 160) * stawka;
+			pensja = (czas * stawka) + nadGodzinki;
+			wyplata.innerText = pensja;
 		}
-		if (workHours > 160) {
-			var nadGodzinki = (workHours - 160) * perHour;
-
-			salary = (workHours * perHour) + nadGodzinki;
-			wyplata.innerText = salary;
-			theBest();
-		}
-		if (workHours <= 100) {
-			salary = workHours * perHour;
-			wyplata.innerText = salary;
-
+		if (czas <= 100) {
+			pensja = czas * stawka;
+			wyplata.innerText = pensja;
 			w.style.background = "red";
 			w.querySelector(".wyplata").style.background = "red";
 			w.querySelector(".stawka").style.background = "red";
 			w.querySelector(".czas").style.background = "red";
-			theBest();
-
 		}
 
-		function theBest() {
-			var employeesSalary = document.querySelector(".wyplata").value;
-			console.log(employeesSalary);
-		}
+		Pracownicy();
 	}
 
+	function Pracownicy() {
+		godziny = 0;
+		//konstruktor obiektu pracownik
+		function Worker(godnosc, godziny, wyplata) {
+			this.godnosc = pracownik;
+			this.godziny = czas;
+			this.wyplata = pensja;
+		}
+		// tworzenie pracownikow - obiektu, tworzenie tablicy obiektow
+		{
+			workers.push({
+				godnosc: this.pracownik,
+				godziny: this.czas,
+				wyplata: this.pensja,
+			});
+		}
 
+	}
+	console.log(workers[10].godziny);
+	console.log(workers[10]);
+	console.log(workers[9]);
+	sort(workers);
+	console.log(workers);
+
+};
+
+function sort(workers) {
+
+
+
+
+	workers.sort(function (a, b) {
+		return a.godziny - b.godziny
+	});
+	console.log(workers);
 }
-/*console.log(workHours);
-console.log(perHour);*/
 
 
 
 
-var button = document.getElementById('oblicz');
-button.addEventListener('click', function () {
-	data();
+
+
+document.getElementById('oblicz').addEventListener('click', function () {
+	oblicz();
 });
