5eda5d0e40343517a0a47800aa80fba63274ce3d
Adrian Bak <adbak97@o2.pl>
2018-06-03 02:52:35 +0200

    Widok responsywny

--- Coacheverest - Adrian Bąk - Kopia/js/js.js
+++ Coacheverest - Adrian Bąk - Kopia/js/js.js
@@ -1,56 +1,36 @@
 $(document).ready(function () {
-
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 200) { // this refers to window
-			console.log('asdsad');
-			$('#myTopNav').addClass("test");
 
+			$('#myTopNav').addClass("test");
 		} else {
 			$('#myTopNav').removeClass('test');
 		}
 	});
-
 	$('#1').on("click", function () {
-
 		$("html, body").animate({
 			scrollTop: 0
 		}, 1000);
 		return false;
-
-
-	})
-
+	});
 	$('#2').on("click", function () {
-
 		$("html, body").animate({
 			scrollTop: 1001
 		}, 1000);
 		return false;
-
-
-	})
+	});
 	$('#3').on("click", function () {
-
 		$("html, body").animate({
 			scrollTop: 2020
 		}, 1000);
 		return false;
-
-
-	})
+	});
 	$('#4').on("click", function () {
-
 		$("html, body").animate({
 			scrollTop: 3050
 		}, 1000);
 		return false;
-
-
-	})
-
-
-
-
+	});
 });
 
 function myFunction() {
